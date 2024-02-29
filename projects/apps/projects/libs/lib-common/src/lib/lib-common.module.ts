e94e120841436722ca24f69395a55632da10f8b8
Darko Vulicevic <darko.vulicevic@enreach.com>
2024-02-29 20:21:45 +0100

    scripts & lib-common works

--- projects/apps/projects/libs/lib-common/src/lib/lib-common.module.ts
+++ projects/apps/projects/libs/lib-common/src/lib/lib-common.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 
+import { TestComponent } from './components/test/test.component';
+
 @NgModule({
   declarations: [
+
+    TestComponent
   ],
   imports: [
   ],
   exports: [
+    TestComponent
   ]
 })
 export class LibCommonModule { }
+
+export {
+    TestComponent
+}
--- projects/apps/projects/libs/lib-common/src/public-api.ts
+++ projects/apps/projects/libs/lib-common/src/public-api.ts
@@ -1,5 +1,4 @@
 /*
  * Public API Surface of lib-common
  */
-
 export * from './lib/lib-common.module';
