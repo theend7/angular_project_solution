75b99f6a0c646cde6a63501f89e6cf43fad110c2
Darko Vulicevic <darko.vulicevic@enreach.com>
2024-03-19 23:00:59 +0100

    app-4 using libcommon, lib10,lib11

--- projects/apps/projects/app-4/src/app/app.module.ts
+++ projects/apps/projects/app-4/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { LibCommonModule } from 'lib-common';
+import { Lib10Module } from 'lib10'
+import { Lib11Module } from 'lib11';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LibCommonModule,
+    Lib10Module,
+    Lib11Module
   ],
   providers: [],
   bootstrap: [AppComponent]
